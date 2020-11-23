e933454cae967b375f4ccd062aa14abd85b1b6bc
AmbreWarrior88 <56254199+AmbreWarrior88@users.noreply.github.com>
2020-11-23 20:28:48 +0100

    fix

--- routes/private.js
+++ routes/private.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const axios = require("axios");
 const User = require("../models/User");
